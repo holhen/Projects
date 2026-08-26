576dd22deaf6f00eaa4ecdc0c1d3dd4bb558e83c
Henrik <henrik.hollosi@labtech.hu>
2026-08-26 15:45:18 +0200

    postgresql

--- Python/Blog With Auth/main.py
+++ Python/Blog With Auth/main.py
@@ -31,7 +31,7 @@ This will install the packages from the requirements.txt for this project.
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 Bootstrap5(app)
 
 # Configure Flask-Login
@@ -49,7 +49,7 @@ def load_user(user_id):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
